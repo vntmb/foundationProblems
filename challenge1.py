0c5bf583f335a74d325464e26db41b9c22d00364
vntmb <tembov@berea.edu>
2016-10-10 21:31:25 -0400

    Create challenge1.py

---
+++ challenge1.py
@@ -0,0 +1,3 @@
+# In this task you will create a program that prints out pascal's triangle (https://upload.wikimedia.org/wikipedia/commons/f/f6/Pascal%27s_triangle_5.svg)
+# A user will give input for how many rows they want the triangle printed out to
+  # The program will print to the commandline pascal's triangle, formatted the same way as in the image
