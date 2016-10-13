6ad4566495cefb2ee2df2cb25c58df1dd9c50753
please18 <gbondos@berea.edu>
2016-10-13 00:34:03 -0400

    Update challenge1.py

--- challenge1.py
+++ challenge1.py
@@ -1,6 +1,7 @@
 # In this task you will create a program that prints out pascal's triangle (https://upload.wikimedia.org/wikipedia/commons/f/f6/Pascal%27s_triangle_5.svg)
 # A user will give input for how many rows they want the triangle printed out to
   # The program will print to the commandline pascal's triangle, formatted the same way as in the image
+#Code by Saffa
 import turtle
 
 tess = turtle.Turtle()
