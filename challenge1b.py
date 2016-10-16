624e607c1ee8a000c81c9a9d0bdf8a4e4c113e40
please18 <gbondos@berea.edu>
2016-10-16 10:58:05 -0400

    Update challenge1b.py

--- challenge1b.py
+++ challenge1b.py
@@ -5,3 +5,36 @@
 #11
 #121
 #1331
+# Coded by Saffa. It is not perfect yet. Any help is appreciated
+import math
+
+
+def exa(z):
+    n = z
+    r = 0
+    fact_list = []
+    while r <= n:
+
+        fact_n = math.factorial(n)
+        fact_r = math.factorial(r)
+        sub = n - r
+        fact_sub = math.factorial(sub)
+        answer = (fact_n / (fact_sub * fact_r))
+        fact_list.append(answer)
+
+        r += 1
+
+    print fact_list
+
+
+def main():
+
+    n = int(raw_input(" Enter n:    "))
+    y = n
+    y += 1
+    for i in range(0, n + 1):
+        y -= 1
+
+        exa(y)
+
+main()
