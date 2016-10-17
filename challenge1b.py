fbbb28af41208479323f90b5ae84e2a7e5446bc3
please18 <gbondos@berea.edu>
2016-10-17 12:57:40 -0400

    Update challenge1b.py

--- challenge1b.py
+++ challenge1b.py
@@ -5,39 +5,38 @@
 #11
 #121
 #1331
-# Coded by Saffa. It is not perfect yet. Any help is appreciated --> Comments in commit message
+# Coded by Saffa. It is now working. 
 import math
 
 
 def exa(z):
     n = z
     r = 0
-    fact_list = []
     example = ""
     while r <= n:
-
         fact_n = math.factorial(n)
         fact_r = math.factorial(r)
         sub = n - r
         fact_sub = math.factorial(sub)
         answer = (fact_n / (fact_sub * fact_r))
-        # fact_list.append(answer)
         example += str(answer) + "  "
 
         r += 1
 
-    # print fact_list
     print example
 
 
 def main():
-
     n = int(raw_input(" Enter n:    "))
     y = n
     y += 1
+    z = []
     for i in range(0, n + 1):
         y -= 1
+        z.append(y)
+    z.reverse()
+    for i in z:
+        exa(i)
 
-        exa(y)
 
 main()
