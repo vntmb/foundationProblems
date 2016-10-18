4707377fd1c29e9f4ec68446ec9559680c1d7203
please18 <gbondos@berea.edu>
2016-10-17 23:17:30 -0400

    Update practice.py

--- practice.py
+++ practice.py
@@ -4,5 +4,28 @@
   # A user can search for any number
   # You need to tell them where (starting at 1, not 0), and how many times the number occurs
   
-  ######################################################
-#COULD YOU PLEASE GIVE AN EXAMPLE OF THE FILE THAT WE SHOULD READ FROM                                      #
+######################################################
+# Hi, Vincent. Please check if this is what you want the code to do ~ Saffa          #
+def read_file(files):
+    f = open(files, "r")
+    r = f.read()
+    z = []
+    for i in r.split():
+        z.append(i)
+    # print z
+    return z
+
+
+def search(first, word):
+
+    print "The word you are searching for \"%s\" occurs %s times" % (word, first.count(word))
+
+
+def main():
+
+    files = raw_input(" What is the name of your file:   ")
+    read_file(files)
+    u_input = raw_input("What word are you searching for:   ")
+    search(read_file(files), u_input)
+    
+main()
